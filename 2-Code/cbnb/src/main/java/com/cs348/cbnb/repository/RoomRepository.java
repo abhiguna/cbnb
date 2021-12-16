77e42174268e87ad59bd73f24718c211435a5173
AnirudhSeela <54326855+AnirudhSeela@users.noreply.github.com>
2021-12-15 20:25:24 -0500

    Update RoomRepository.java

--- 2-Code/cbnb/src/main/java/com/cs348/cbnb/repository/RoomRepository.java
+++ 2-Code/cbnb/src/main/java/com/cs348/cbnb/repository/RoomRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    public List<Room> findRoomsByDormId(String dormId);
 }
