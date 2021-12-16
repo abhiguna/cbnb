1a78eaeadb099da37615ee79aba9ce05168dbeb0
AnirudhSeela <54326855+AnirudhSeela@users.noreply.github.com>
2021-12-15 20:31:14 -0500

    Update db_schema.sql

--- 2-Code/db_schema.sql
+++ 2-Code/db_schema.sql
@@ -2,31 +2,28 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS University;
 DROP TABLE IF EXISTS Dorm;
 DROP TABLE IF EXISTS Room;
-DROP TABLE IF EXISTS Waitlist;
 DROP TABLE IF EXISTS Booking;
 DROP TABLE IF EXISTS Guest;
 
 CREATE TABLE University (
-	univ_id INT NOT NULL,
-	name VARCHAR(30),
+	name VARCHAR(30) NOT NULL,
 	city VARCHAR(20),
 	state VARCHAR (20),
 	country VARCHAR (20),
 	building_cnt INT,
 	guest_cnt INT,
-	PRIMARY KEY(univ_id)
+	PRIMARY KEY(name)
 );
 
 CREATE TABLE Dorm (
-	dorm_id INT NOT NULL,
-	uID INT,
-	name VARCHAR(30),
+	name VARCHAR(30) NOT NULL,
+	uID VARCHAR(30),
 	address VARCHAR (100),
 	total_guests INT,
 	rooms_available INT,
 	dorm_type VARCHAR(10), /* boys | girls | all */
-	PRIMARY KEY(dorm_id),
-	FOREIGN KEY(uID) REFERENCES University(univ_id) ON DELETE CASCADE
+	PRIMARY KEY(name),
+	FOREIGN KEY(uID) REFERENCES University(name) ON DELETE CASCADE
 );
 
 CREATE TABLE Room (
@@ -39,18 +36,6 @@ CREATE TABLE Room (
     max_capacity INT,
     PRIMARY KEY(room_id),
     FOREIGN KEY(dID) REFERENCES Dorm(name) ON DELETE CASCADE
-);
-
-CREATE TABLE Waitlist (
-	waitlist_id INT NOT NULL,
-	gID INT,
-	rID INT,
-	dID INT,
-	requested_date DATE,
-	total_in_queue INT,
-	pos_in_queue INT,
-	PRIMARY KEY(waitlist_id),
-	FOREIGN KEY(gID, rID, dID, requested_date) REFERENCES Booking(gID, rID, dID, booking_date)
 );
 
 CREATE TABLE Booking (
